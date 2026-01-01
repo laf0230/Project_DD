33e23f884598b4acc3f711edbcd3eb0b54336e22
laf <goodays1003@gmail.com>
2026-01-02 01:26:46 +0900

    feat: passage

---
+++ Assets/Talk System/Script/CharacterTalk.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Talk
+{
+    [AddComponentMenu("대화/대화 오브젝트")]
+    public class CharacterTalk : MonoBehaviour
+    {
+        [Header("활성화 시킬 대화 이름")]
+        [Tooltip("이벤트를 통해서 대화를 교체하고 싶다면 \"인터랙션  반응 이벤트\"를 이용해주세요!")]
+        [SerializeField] string dialogueId = "Sample";
+        public bool isTalkable = true;
+
+        public void StartDialogue()
+        {
+            TalkManager.Instance.ActiveDialogue(dialogueId);
+        }
+
+        public void ChangeDialogueID(string id) => dialogueId = id;
+
+        public void SetTalkable(bool isTalkable) => this.isTalkable = isTalkable;
+    }
+}
---
+++ Assets/script/Debug/DistanceDubug.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DistanceDubug : MonoBehaviour
+{
+    public Color color = Color.white;
+    public float distance = 0;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(transform.position, distance);
+    }
+}
